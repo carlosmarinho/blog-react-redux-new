02e93ea0946278b2452d6e7b84b77aced093de3e
carlosmarinho <carluizfla@hotmail.com>
2019-08-27 01:10:42 -0300

    adicionado o redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,27 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async() => {
-    //BAD APPROACH!!!
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    
+    dispatch({ type: 'FETCH_POSTS', payload: response }) 
+}
+
+    /*BAD APPROACH!!! it will get an error
+    because we have to return a plain javascript object
     const response = await jsonPlaceholder.get('/posts');
     
     return {
         type: 'FETCH_POSTS',
         payload: response
     }
+
+    //IF I take way the async await it will take too long to return
+    because its an asynchronous request
+    const response = jsonPlaceholder.get('/posts');
+    
+    return {
+        type: 'FETCH_POSTS',
+        payload: response
+    }
+    */
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.querySelector('#root')
+)
+
+/* WITHOUT REDUX THUNK 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
@@ -12,3 +31,4 @@ ReactDOM.render(
     </Provider>, 
     document.querySelector('#root')
 )
+*/
