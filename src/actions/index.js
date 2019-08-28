8819c41a61460b1fafab77a27606e052c5ecc657
Carlos Marinho <carluizfla@hotmail.com>
2019-08-28 17:03:17 -0300

    refactor using memoise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,21 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
     
-    dispatch({ type: 'FETCH_POSTS', payload: response }) 
+    dispatch({ type: 'FETCH_POSTS', payload: response.data }) 
 }
+
+
+export const fetchUser = (id) => async dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data })
+});
+
 
     /*BAD APPROACH!!! it will get an error
     because we have to return a plain javascript object
@@ -24,4 +35,3 @@ export const fetchPosts = () => async dispatch => {
         payload: response
     }
     */
-}
